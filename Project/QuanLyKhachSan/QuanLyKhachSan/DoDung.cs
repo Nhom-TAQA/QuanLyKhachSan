726a392530acd09a4ed4884ab3bfb11c174cf907
Be003931 <66206101+Be003931@users.noreply.github.com>
2020-12-10 16:48:53 +0700

    Revert "Revert "Hoàng Anh""
    This reverts commit 17c7c4b3645307b6cb49e5bc384e625a17978bfe.

--- Project/QuanLyKhachSan/QuanLyKhachSan/DoDung.cs
+++ Project/QuanLyKhachSan/QuanLyKhachSan/DoDung.cs
@@ -26,42 +26,159 @@ namespace QuanLyKhachSan
 
         private void reset()
         {
-            
+            listView1.Items.Clear();
+            table = new DataTable();
+            table = connector.LoadData("3");
+            int i = 0;
+            foreach (DataRow row in table.Rows)
+            {
+                listView1.Items.Add(row[0].ToString());
+                listView1.Items[i].SubItems.Add(row[1].ToString());
+                i++;
+            }
         }
 
         private void reset2()
         {
-            
+            listView1.Items.Clear();
+            table = connector.FindObject("3", textSearch.Text.Trim());
+            int i = 0;
+            foreach (DataRow row in table.Rows)
+            {
+                listView1.Items.Add(row[0].ToString());
+                listView1.Items[i].SubItems.Add(row[1].ToString());
+                i++;
+            }
         }
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            if (listView1.SelectedItems.Count > 0)
+            {
+                int index = listView1.SelectedItems[0].Index;
+                textID.Text = table.Rows[index][0].ToString();
+                textName.Text = table.Rows[index][1].ToString();
+                textPrice.Text = table.Rows[index][2].ToString();
+            }
         }
 
         private void radView_CheckedChanged(object sender, EventArgs e)
         {
-           
+            RadioButton rad = (RadioButton)(sender);
+            if (rad.Checked == true)
+            {
+                butFree.Text = rad.Text;
+                if (rad.Text == "Xem")
+                {
+                    butFree.Enabled = false;
+                    textPrice.ReadOnly = textName.ReadOnly = textID.ReadOnly = true;
+                }
+                else
+                {
+                    butFree.Enabled = true;
+                    textID.ReadOnly = true;
+                    textPrice.ReadOnly = textName.ReadOnly = false;
+                    if (rad.Text == "Thêm")
+                    {
+                        textID.ReadOnly = false;
+                        textPrice.Text = textName.Text = textID.Text = "";
+                    }
+                }
+            }
         }
 
         private void butDel_Click(object sender, EventArgs e)
         {
-            
+            if (listView1.SelectedItems.Count > 0)
+            {
+                connector.DeleteObject("3", textID.Text.Trim(), "");
+                if (butSearch.Text.Trim() == "Hủy")
+                {
+                    reset2();
+                }
+                else reset();
+                textPrice.Text = textName.Text = textID.Text = "";
+                MessageBox.Show("Xóa thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
 
         private void butSearch_Click(object sender, EventArgs e)
         {
-            
+            if (butSearch.Text.Trim() == "Tìm")
+            {
+                if (textSearch.Text.Trim() == "")
+                {
+                    MessageBox.Show("Text box is empty!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    textSearch.Focus();
+                    return;
+                }
+                butSearch.Text = "Hủy";
+                reset2();
+            }
+            else
+            {
+                butSearch.Text = "Tìm";
+                reset();
+            }
         }
         private bool check(int k)
         {
-           
+            if (k == 1)
+            {
+                if (textID.Text.Trim() == "")
+                {
+                    MessageBox.Show("ID's values must not be null", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    textID.Focus();
+                    return false;
+                }
+                for (int i = 0; i < table.Rows.Count; i++)
+                {
+                    if (textID.Text.Trim() == table.Rows[i][0].ToString().Trim())
+                    {
+                        MessageBox.Show("ID's values has been taken, please input new ID", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                        textID.Focus();
+                        return false;
+                    }
+                }
+            }
+            if (textName.Text.Trim() == "")
+            {
+                MessageBox.Show("State's values must not be null", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                textName.Focus();
+                return false;
+            }
+            if (textPrice.Text.Trim() == "")
+            {
+                MessageBox.Show("Price's values must not be null", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                textPrice.Focus();
+                return false;
+            }
+            int b;
+            if (int.TryParse(textPrice.Text, out b) == false)
+            {
+                MessageBox.Show("This Price per day value is not valid", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                textPrice.Focus();
+                return false;
+            }
             return true;
         }
 
         private void butFree_Click(object sender, EventArgs e)
         {
-           
+            if (butFree.Text == "Thêm")
+            {
+                if (check(1) == false) return;
+                connector.InsertUpdateObject("AddObject", "3", textID.Text, "", textName.Text, "1/1/1994", "1/1/1994", textPrice.Text);
+                MessageBox.Show("Thêm thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                if (check(2) == false) return;
+                connector.InsertUpdateObject("EditObject", "3", textID.Text, "", textName.Text, "1/1/1994", "1/1/1994", textPrice.Text);
+                MessageBox.Show("Sửa thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (butSearch.Text.Trim() == "Tìm") reset();
+            else reset2();
         }
     }
 }
--- Project/QuanLyKhachSan/QuanLyKhachSan/KhachHang.cs
+++ Project/QuanLyKhachSan/QuanLyKhachSan/KhachHang.cs
@@ -27,42 +27,166 @@ namespace QuanLyKhachSan
 
         private void reset()
         {
-            
+            listView1.Items.Clear();
+            table = new DataTable();
+            table = connector.LoadData("1");
+            int i = 0;
+            foreach (DataRow row in table.Rows)
+            {
+                listView1.Items.Add(row[0].ToString());
+                listView1.Items[i].SubItems.Add(row[1].ToString());
+                i++;
+            }
         }
 
         private void reset2()
         {
-            
+            listView1.Items.Clear();
+            table = connector.FindObject("1", textSearch.Text.Trim());
+            int i = 0;
+            foreach (DataRow row in table.Rows)
+            {
+                listView1.Items.Add(row[0].ToString());
+                listView1.Items[i].SubItems.Add(row[1].ToString());
+                i++;
+            }
         }
 
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            if (listView1.SelectedItems.Count > 0)
+            {
+                int index = listView1.SelectedItems[0].Index;
+                textID.Text = table.Rows[index][0].ToString();
+                textName.Text = table.Rows[index][1].ToString();
+                textCMND.Text = table.Rows[index][2].ToString();
+                textPhone.Text = table.Rows[index][3].ToString();
+            }
         }
 
         private void radView_CheckedChanged(object sender, EventArgs e)
         {
-            
+            RadioButton rad = (RadioButton)(sender);
+            if (rad.Checked == true)
+            {
+                butFree.Text = rad.Text;
+                if (rad.Text == "Xem")
+                {
+                    butFree.Enabled = false;
+                    textCMND.ReadOnly = textName.ReadOnly = textID.ReadOnly = textPhone.ReadOnly = true;
+                }
+                else
+                {
+                    butFree.Enabled = true;
+                    textID.ReadOnly = true;
+                    textCMND.ReadOnly = textName.ReadOnly = textPhone.ReadOnly = false;
+                    if (rad.Text == "Thêm")
+                    {
+                        textID.ReadOnly = false;
+                        textCMND.Text = textName.Text = textID.Text = textPhone.Text = "";
+                    }
+                }
+            }
         }
 
         private void butDel_Click(object sender, EventArgs e)
         {
-            
+            if (listView1.SelectedItems.Count > 0)
+            {
+                connector.DeleteObject("1", textID.Text.Trim(), "");
+                if (butSearch.Text.Trim() == "Hủy")
+                {
+                    reset2();
+                }
+                else reset();
+                textCMND.Text = textName.Text = textID.Text = textPhone.Text = "";
+                MessageBox.Show("Xóa thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
 
         private void butSearch_Click(object sender, EventArgs e)
         {
-            
+            if (butSearch.Text.Trim() == "Tìm")
+            {
+                if (textSearch.Text.Trim() == "")
+                {
+                    MessageBox.Show("Nhập thông tin cần tìm!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    textSearch.Focus();
+                    return;
+                }
+                butSearch.Text = "Hủy";
+                reset2();
+            }
+            else
+            {
+                butSearch.Text = "Tìm";
+                reset();
+            }
         }
         private bool check(int k)
         {
-            
+            if (k == 1)
+            {
+                if (textID.Text.Trim() == "")
+                {
+                    MessageBox.Show("Không được để trống Mã khách hàng", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    textID.Focus();
+                    return false;
+                }
+                for (int i = 0; i < table.Rows.Count; i++)
+                {
+                    if (textID.Text.Trim() == table.Rows[i][0].ToString().Trim())
+                    {
+                        MessageBox.Show("Mã khách hàng đã tồn tại", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                        textID.Focus();
+                        return false;
+                    }
+                }
+            }
+            if (textName.Text.Trim() == "")
+            {
+                MessageBox.Show("Họ tên khách hàng không được phép để trống", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                textName.Focus();
+                return false;
+            }
+            if (textCMND.Text.Trim() == "")
+            {
+                MessageBox.Show("CMND không được bỏ trống", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                textCMND.Focus();
+                return false;
+            }
+            if (textPhone.Text.Trim() == "")
+            {
+                MessageBox.Show("SĐT không được để trống", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                textPhone.Focus();
+                return false;
+            }
+            int b;
+            if (int.TryParse(textPhone.Text, out b) == false)
+            {
+                MessageBox.Show("SĐT không hợp lệ", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                textPhone.Focus();
+                return false;
+            }
             return true;
         }
 
         private void butFree_Click(object sender, EventArgs e)
         {
-            
+            if (butFree.Text == "Thêm")
+            {
+                if (check(1) == false) return;
+                connector.InsertUpdateObject("AddObject", "1", textID.Text, textCMND.Text, textName.Text, "1/1/1994", "1/1/1994", textPhone.Text);
+                MessageBox.Show("Thêm thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                if (check(2) == false) return;
+                connector.InsertUpdateObject("EditObject", "1", textID.Text, textCMND.Text, textName.Text, "1/1/1994", "1/1/1994", textPhone.Text);
+                MessageBox.Show("Sửa thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (butSearch.Text.Trim() == "Tìm") reset();
+            else reset2();
         }
     }
 }
