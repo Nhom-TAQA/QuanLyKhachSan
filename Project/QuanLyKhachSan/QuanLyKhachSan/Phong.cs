ee0bbdd578b9ad969aea5a29b99d7b0983189fde
Be003931 <66206101+Be003931@users.noreply.github.com>
2020-12-20 17:03:54 +0700

    a

--- Project/QuanLyKhachSan/QuanLyKhachSan/Phong.cs
+++ Project/QuanLyKhachSan/QuanLyKhachSan/Phong.cs
@@ -17,7 +17,10 @@ namespace QuanLyKhachSan
             InitializeComponent();
         }
 
-       
+       private void FormPhong_Load(object sender, EventArgs e)
+        {
+            reset();
+        }
 
         private DataTable table;
         private Connection connector = new Connection();
@@ -31,10 +34,7 @@ namespace QuanLyKhachSan
         {
            
         }
-	private void FormPhong_Load(object sender, EventArgs e)
-        {
-            reset();
-        }
+	
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
            
