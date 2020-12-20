49d3f38bb3bfd71e9e0e4d2e9854269d5325e2ff
Be003931 <66206101+Be003931@users.noreply.github.com>
2020-12-20 17:07:42 +0700

    Update Phong.Designer.cs

--- Project/QuanLyKhachSan/QuanLyKhachSan/Phong.Designer.cs
+++ Project/QuanLyKhachSan/QuanLyKhachSan/Phong.Designer.cs
@@ -30,7 +30,6 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Phong));
             //lam ve Phong
-            
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1256, 577);
