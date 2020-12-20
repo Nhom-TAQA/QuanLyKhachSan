da8ef44703844f4f229451ac2a172ef0edc5276f
Be003931 <66206101+Be003931@users.noreply.github.com>
2020-12-20 17:06:34 +0700

    Update Phong.Designer.cs

--- Project/QuanLyKhachSan/QuanLyKhachSan/Phong.Designer.cs
+++ Project/QuanLyKhachSan/QuanLyKhachSan/Phong.Designer.cs
@@ -32,7 +32,7 @@
             
             
             // Phong
-            // 
+            
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1256, 577);
